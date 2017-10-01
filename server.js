ce2fe75ef02c24efbcde928dc3fa1ec68e2e7d1f
John Desrosiers <johndrunner@yahoo.com>
2017-10-01 14:32:35 -0700

    implement routes in server and update code organization of the file

--- server.js
+++ server.js
@@ -4,25 +4,26 @@
 const Express = require("express");
 const BodyParser = require("body-parser");
 const Exphbs = require("express-handlebars");
-const DB = require("./models");
-// const Routes = require("./controllers/controller.js");
 
 let app = Express();
 let PORT = process.env.PORT || 8080;
+
+// require models
+const DB = require("./models");
+
+// Parse application/x-www-form-urlencoded
+app.use(BodyParser.urlencoded({ extended: false }));
 
 // Serve static content for the app from the "public" directory in 
 // the application directory.
 app.use(Express.static("public"));
 
-// Parse application/x-www-form-urlencoded
-app.use(BodyParser.urlencoded({ extended: false }));
-
 // Setup Handlebars
 app.engine("handlebars", Exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// connecting router
-// app.use("/", Routes);
+// connect routes
+require("./routes/api-routes-races")(app);
 
 // sync the database and start server listening
 DB.sequelize.sync().then(() => {
