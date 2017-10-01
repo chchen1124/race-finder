89bd7fc4a4f1f6b416527f41fa8280e3dc65637b
John Desrosiers <johndrunner@yahoo.com>
2017-10-01 09:37:34 -0700

    set up zip_code as the primary key for locations and no null values allowed

--- models/locations.js
+++ models/locations.js
@@ -8,24 +8,28 @@ const Sequelize = require("sequelize");
 const sequelize = require("../config/connection.js");
 
 // Define location model
-const Location = sequelize.define("locations", {
+const Locations = sequelize.define("locations", {
+	// zip code is the primary key
 	zip_code: {
 		// uses 5 digit zip codes. use string for zips that begin with 0.
-		type: Sequelize.STRING(5)
+		type: Sequelize.STRING(5),
+		allowNull: false,
+		primaryKey: true,
+		unique: true
 	},
 	city: {
 		// limit the city name length to 25 characters
 		// the longest city name in the us is 22 characters (Truth or Consequences, NM)
-		type: Sequelize.STRING(25)
+		type: Sequelize.STRING(25),
+		allowNull: false
 	},
 	state: {
 		// use 2 character postal abreviations for states
-		type: Sequelize.STRING(2)
+		type: Sequelize.STRING(2),
+		allowNull: false
 	}
-}, {
-	timestamps: false
 });
 location.sync();
 
 // export the Location model
-module.exports = Location;
+module.exports = Locations;
