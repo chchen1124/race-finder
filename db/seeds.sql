d6130be4d39c3c607d1af31c0b91b7e0a99320c4
John Desrosiers <johndrunner@yahoo.com>
2017-10-01 14:19:05 -0700

    fixed seeds file for update to model

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,8 +5,9 @@ INSERT INTO locations (zip_code,city,state, createdAt, updatedAt) VALUES
 ("24098",'Long Beach','CA', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 ("65590",'Gilbert','AZ', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
-INSERT INTO races (name, url, zip_code, race_date, avg_temp, createdAt, updatedAt) VALUES 
+INSERT INTO races (name, url, LocationZipCode, race_date, avg_temp, createdAt, updatedAt) VALUES 
 ('10 for Texas','https://mybestruns.com/10forTexas',"77380",'2017-10-14',13.75, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 ('Jet Blue Long Beach Marathon & Half Marathon','https://mybestruns.com/LongBeachInternationalCi',"24098",'2017-10-08',15.13, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
 ('12 ks of Christmas Run','https://mybestruns.com/12ksofChristmasRun',"65590",'2017-12-09',18.21, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
+	
