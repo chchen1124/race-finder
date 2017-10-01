fabe6fa985087203df8b562ac4001810ed153f73
John Desrosiers <johndrunner@yahoo.com>
2017-10-01 09:14:39 -0700

    fix white space

--- models/races.js
+++ models/races.js
@@ -2,34 +2,34 @@
 // =============================================================
 
 // Sequelize (capital) references the standard library
-var Sequelize = require("sequelize");
+let Sequelize = require("sequelize");
 // sequelize (lowercase) references my connection to the DB.
-var sequelize = require("../config/connection.js");
+let sequelize = require("../config/connection.js");
 
 // Creates a "Race" model that matches up with DB
-var Race = sequelize.define("races", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  name: {
-    type: Sequelize.STRING
-  },
-  url: {
-    type: Sequelize.STRING
-  },
-  zip_code: {
-    type: Sequelize.INTEGER
-  },
-  date:{
-    type:Sequelize.DATEONLY
-  },
-  avg_temp:{
-    type:Sequelize.DOUBLE
-  }
+let Race = sequelize.define("races", {
+	id: {
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		primaryKey: true
+	},
+	name: {
+		type: Sequelize.STRING
+	},
+	url: {
+		type: Sequelize.STRING
+	},
+	zip_code: {
+		type: Sequelize.INTEGER
+	},
+	date:{
+		type:Sequelize.DATEONLY
+	},
+	avg_temp:{
+		type:Sequelize.DOUBLE
+	}
 }, {
-  timestamps: false
+	timestamps: false
 });
 
 // Syncs with DB
