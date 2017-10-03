cb794bc7f63b3d08153a4cb5f523d791cc607c60
John Desrosiers <johndrunner@yahoo.com>
2017-10-03 07:10:44 -0700

    race url now returned from post api

--- controllers/api-routes-races.js
+++ controllers/api-routes-races.js
@@ -69,6 +69,7 @@ module.exports = function(app) {
 				state: firstRace.Location.state,
 				name: firstRace.name,
 				temp: null,
+				url: firstRace.url,
 				date: mDate.format("M/D/YYYY")
 			};
 
