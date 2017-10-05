6091bd68692f485aa700dfa51b0ccb78ac7aff35
John Desrosiers <johndrunner@yahoo.com>
2017-10-04 19:08:21 -0700

    test get / route with chai

--- test/test.js
+++ test/test.js
@@ -1,27 +1,28 @@
-// set the environment to test
-process.env.NODE_ENV = 'test';
-
 // dependencies for testing
-const assert = require("assert");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const should = chai.should();
+
+// set the environment to test
+process.env.NODE_ENV = 'test';
 const server = require("../server.js");
 
+chai.use(chaiHttp);
 
 // Special rules for eslint //
 /* eslint func-names: "off", prefer-arrow-callback: "off" */
-/* globals describe it before */
+/* globals describe expect it */
 
 // test the server connection
 describe("server", function () {
     describe("GET /", function () {
-        it("should return a web page", function (done) {
+        it("should return ok status", function (done) {
             chai.request(server)
                 .get("/")
                 .end(function (err, res) {
+                    chai.expect(err).to.be.an("null");
                     res.should.have.status(200);
-                    res.body.should.be.a("string");
+                    done();
                 });
         });
     });
